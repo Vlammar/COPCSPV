b141d45b0391a828d159d04b1f534dd91b1bf6fd
root <root@Pc-valentin.localdomain>
2020-01-03 18:40:11 +0100

    update for submodules

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #Made by Valentin Jabre
 #build the csp modeler
+cp src/Frequences.java CSP/XCSP3-Java-Tools/src/main/java/org/xcsp/modeler/problems
 cd CSP/XCSP3-Java-Tools
 mvn package -Dmaven.test.skip=true
 cd ../../..
